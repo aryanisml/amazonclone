47aa27cb50596b6acb3ae7d58bf2b5e87c081bf0
swapnil <swapnil.kamble02@gmail.com>
2021-03-02 02:23:30 -0800

    build fix

--- src/useProducts.js
+++ src/useProducts.js
@@ -7,15 +7,15 @@ const useProducts = () => {
 
   useEffect(() => {
     const getProducts = async () => {
-      const headers = {
-        "content-type": "application/x-www-form-urlencoded",
-        "x-rapidapi-key": "94daf8581dmsh36d3b6116d51e1fp12c9c7jsn55ec2e17b3d3",
-        "x-rapidapi-host": "BestBuyraygorodskijV1.p.rapidapi.com",
-        "useQueryString": true
-      };
-      const params = {
-          apiKey : 'itDA4GN4tXHqxpGYhZxu9wxj'
-      }
+      // const headers = {
+      //   "content-type": "application/x-www-form-urlencoded",
+      //   "x-rapidapi-key": "94daf8581dmsh36d3b6116d51e1fp12c9c7jsn55ec2e17b3d3",
+      //   "x-rapidapi-host": "BestBuyraygorodskijV1.p.rapidapi.com",
+      //   "useQueryString": true
+      // };
+      // const params = {
+      //     apiKey : 'itDA4GN4tXHqxpGYhZxu9wxj'
+      // }
       const result = await axios.get(
         `https://api.bestbuy.com/v1/products((categoryPath.id=pcmcat209400050001))?apiKey=itDA4GN4tXHqxpGYhZxu9wxj&format=json`
       );
